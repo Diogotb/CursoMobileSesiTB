919c10bee997785519a434287c22338a0129ec1e
Diogotb <digogo@gmail.com>
2025-08-14 13:06:13 -0300

    14/08

--- JsonConvert/Exemplo1JsonConvert.dart
+++ JsonConvert/Exemplo1JsonConvert.dart
@@ -3,6 +3,7 @@
 import 'dart:convert'; //biblioteca para funcionamento do Json
 
 void main() {
+  // Texto em Formato Json
   String dbJson = '''{
             "id":1,
             "nome": "João",
@@ -11,6 +12,7 @@ void main() {
             "senha": 1234
                 }''';
 
+  // convertendo Texto Json -> Map Dart
   Map<String,dynamic> usuario = json.decode(dbJson);
 
   print(usuario["login"]); //joao_user
@@ -19,7 +21,7 @@ void main() {
 
   usuario["senha"] = 1111;
 
-  // fazer o encode
+  // fazer o encode -> Map Dart -> Texto Json
 
   dbJson = json.encode(usuario);
 
